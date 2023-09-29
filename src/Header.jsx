25042ffe70dd47221c0c87188a0b99c306cf3b57
Ebru <yerdebru@gmail.com>
2023-09-29 14:46:46 +0300

    change

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,7 @@
 export default function Header() {
   return (
     <div className="header-cont">
-      <img
-        className="header-icon"
-        src="src/assets/Fill213.png"
-        alt="header-icon"
-      />
+      <img className="header-icon" src="/Fill213.png" alt="header-icon" />
       <p className="header-text">my travel journal.</p>
     </div>
   );
--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -3,14 +3,14 @@ export default function MainSection(props) {
     <div className="dest1">
       <img
         className="dest1-img"
-        src={`src/assets/${props.data.imageUrl}`}
+        src={`/${props.data.imageUrl}`}
         alt="dest1-img"
       />
       <div className="info">
         <div className="location-cont">
           <img
             className="location-icon"
-            src="src/assets/Fill219.png"
+            src="/Fill219.png"
             alt="location-icon"
           />
           <h3 className="country">{props.data.location}</h3>
